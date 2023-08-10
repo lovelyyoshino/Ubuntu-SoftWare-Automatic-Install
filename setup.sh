101c6f1b60e6dd4d4fa75952b0464e3cf34e2765
lovelyyoshino <mpl9725440@gmail.com>
2023-08-10 16:34:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -123,11 +123,11 @@ cd dash-to-dock
 make 
 make install
 
-# install nodejs and npm
-echo "configure nodejs and npm environment"
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+# # install nodejs and npm
+# echo "configure nodejs and npm environment"
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+# sudo apt-get install -y build-essential
 
 #install markdown editor tepora
 echo "install markdown editor Typora"
@@ -139,19 +139,19 @@ sudo apt-get install typora
 
 
 # install oracle jdk
-echo "ready configure oracle java jdk"
-jdkContainer="jdk.tar.gz"
-cd ~/Downloads
-sudo wget -O ${jdkContainer} --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
-tar -xvzf ${jdkContainer}
-sudo mv  ~/Downloads/jdk1.8.0_151 ${swDir}/jdk
+# echo "ready configure oracle java jdk"
+# jdkContainer="jdk.tar.gz"
+# cd ~/Downloads
+# sudo wget -O ${jdkContainer} --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+# tar -xvzf ${jdkContainer}
+# sudo mv  ~/Downloads/jdk1.8.0_151 ${swDir}/jdk
 
-sudo echo "export JAVA_HOME=${swDir}/jdk" >> /etc/profile
-sudo echo "export JRE_HOME=\${JAVA_HOME}/jre" >> /etc/profile
-sudo echo "export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib" >> /etc/profile
-sudo echo "export PATH=\${JAVA_HOME}/bin:\$PATH" >> /etc/profile
-source /etc/profile
-echo "finish configure oracle java jdk"
+# sudo echo "export JAVA_HOME=${swDir}/jdk" >> /etc/profile
+# sudo echo "export JRE_HOME=\${JAVA_HOME}/jre" >> /etc/profile
+# sudo echo "export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib" >> /etc/profile
+# sudo echo "export PATH=\${JAVA_HOME}/bin:\$PATH" >> /etc/profile
+# source /etc/profile
+# echo "finish configure oracle java jdk"
 
 
 
@@ -162,12 +162,13 @@ echo "#######################################################################"
 echo ""
 
 
-vsCodeLink="https://az764295.vo.msecnd.net/stable/507ce72a4466fbb27b715c3722558bb15afa9f48/code_1.57.1-1623937013_amd64.deb"
-virtualBoxLink="http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~xenial_amd64.deb"
+vsCodeLink="https://az764295.vo.msecnd.net/stable/6445d93c81ebe42c4cbd7a60712e0b17d9463e97/code_1.81.0-1690980880_amd64.deb"
+#virtualBoxLink="http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~xenial_amd64.deb"
 #osxArcCollectionThemeLink="https://github-production-release-asset-2e65be.s3.amazonaws.com/77880841/16a14c7c-45a6-11e7-81ac-28673f670d57?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20171022%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20171022T093955Z&X-Amz-Expires=300&X-Amz-Signature=98b29dcd8849047f0e774fa1dd00353c8d8c60e4927c6273aa9afba5f5e3d14b&X-Amz-SignedHeaders=host&actor_id=22359905&response-content-disposition=attachment%3B%20filename%3Dosx-arc-collection_1.4.3_amd64.deb"
 googleChromeLink="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sougouLink="http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=bBYOyY4OxnTa-_ElgJuKDw&e=1508784697&fn=sogoupinyin_2.1.0.0086_amd64.deb"
+sougouLink="https://ime-sec.gtimg.com/202308101244/09101fe56485bfec2d2f78d54a968fc2/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb"
 netMusicLink="http://s1.music.126.net/download/pc/netease-cloud-music_1.0.0-2_amd64_ubuntu16.04.deb"
+utoolsLink="https://res.u-tools.cn/version2/utools_4.0.0_amd64.deb"
 
 
 
@@ -182,10 +183,6 @@ vscodeName="vsCode.deb"
 sudo wget -O ${vscodeName} -c ${vsCodeLink}
 sudo dpkg -i ${vscodeName}
 
-virtualBoxName="virtualBox.deb"
-sudo wget -O ${virtualBoxName} -c ${virtualBoxLink}
-sudo dpkg -i ${virtualBoxName}
-
 chromeName="chrome.deb"
 sudo wget -O ${chromeName} -c ${googleChromeLink}
 sudo dpkg -i ${chromeName}
@@ -193,6 +190,10 @@ sudo dpkg -i ${chromeName}
 netMusicName="netMusic.deb"
 sudo wget -O ${netMusicName} -c ${netMusicLink}
 sudo dpkg -i ${netMusicName}
+
+utoolsName="utools.deb"
+sudo wget -O ${utoolsName} -c ${utoolsLink}
+sudo dpkg -i ${utoolsName}
 
 #http link error
 #osxArcName="osxArc.deb"
@@ -206,12 +207,6 @@ sudo wget -O ${genymotionName} --no-check-certificate -c ${genymotionLink}
 chmod +x ${genymotionName}
 sudo ./${genymotionName}
 
-#install intellij
-sudo wget -c ${intellijIdeaLink}
-tar -xvzf ideaIU-2017.2.5.tar.gz
-mv idea-IU-172.4343.14 ${swDir}/idea-IU-172.4343.14
-cd ${swDir}/idea-IU-172.4343.14/bin
-./idea.sh 
 
 echo ""
 echo "#######################################################################"
